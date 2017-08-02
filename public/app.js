abd7ffe75f53e3184c1b64acafe5f12d5416a311
Jia Wong <jpw_4@msn.com>
2017-08-02 07:42:50 +0100

    other location and info box click

--- public/app.js
+++ public/app.js
@@ -1,15 +1,37 @@
 var initialize = function(){
   var center = {lat: 55.857103, lng: -4.243951};
-  var zoom = 18;
+  var zoom = 15;
   var mapDiv = document.querySelector("#main-map");
   var mainMap = new MapWrapper(mapDiv, center, zoom);
-
   mainMap.addMarker(center);
-
   mainMap.addClickEvent();
 
   var button = document.querySelector("#bounce-button");
   button.addEventListener("click", mainMap.bounceMarkers.bind(mainMap));
+
+  var christiePark = {
+    lat: 55.989661,
+      lng: -4.587840
+    };
+
+    var contentString = '<div id = "content">' +
+    '<h1>Christie Park</h1>'+
+    '<p>Christie Park lies in the heart of the village of Alexandria near the A82.</p>' +
+    '<br/>'+
+    '<h3>Jia lost her frisbee here at the age of 16 :)</h3>'+
+    '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+  var parkButton = document.getElementById('park-button');
+  var christieParkMarker = mainMap.addMarker(christiePark);
+
+  google.maps.event.addListener(christieParkMarker, 'click',  function(){
+    infowindow.open(mainMap, christieParkMarker);
+  })
+  parkButton.addEventListener('click', mainMap.move.bind(mainMap)); 
 }
+
 
 window.addEventListener("load", initialize);
--- public/mapwrapper.js
+++ public/mapwrapper.js
@@ -6,13 +6,22 @@ var MapWrapper = function(container, center, zoom){
   this.markers = [];
 }
 
-MapWrapper.prototype.addMarker = function(coords){
+MapWrapper.prototype.addMarker = function(coords, onclick){
   var marker = new google.maps.Marker({
     position: coords,
     map: this.googleMap
   });
   this.markers.push(marker);
+
+  google.maps.event.addListener(marker, 'click', onclick);
 }
+
+var contentString = '<div id = "content">' +
+'<h1>Christie Park</h1>'+
+'<p>Christie Park lies in the heart of the village of Alexandria near the A82.</p>' +
+'<br/>'+
+'<h3>Jia lost her frisbee here at the age of 16 :)</h3>'+
+'</div>';
 
   MapWrapper.prototype.addClickEvent = function(){
     google.maps.event.addListener(this.googleMap, 'click', function(event){
@@ -24,6 +33,7 @@ MapWrapper.prototype.addMarker = function(coords){
       }
       this.addMarker(coords);
     }.bind(this));
+
   };
 
   MapWrapper.prototype.bounceMarkers = function(){
@@ -31,3 +41,18 @@ MapWrapper.prototype.addMarker = function(coords){
       marker.setAnimation(google.maps.Animation.BOUNCE);
     })
   };
+
+  MapWrapper.prototype.move = function(){
+    var christiePark = {
+      lat: 55.989661,
+        lng: -4.587840
+      };
+      this.googleMap.setCenter(christiePark);
+
+};
+
+  
+  // Add an Info Window to one of the markers on your map (see the Google Maps API documentation) with some information about the place it's marking.
+
+  // Add a button below the map that says 'Take me to Chicago' (or another city) which moves the center of the map to be the co-ordinates of that city. Tip: look up the 'setCenter' method for Google Maps.
+
